9b9428d82a142382d5aa8a533052a40bfcb069a9
adityadevraj699 <aditya.kumar1.cs.2022@mitmeerut.ac.in>
2026-04-14 11:53:59 +0530

    feat: Add Loader, MovieCard, and MovieRow components for enhanced UI
    feat: Implement Navbar and SearchBar components for navigation and search functionality
    
    feat: Create Category and Home pages to display movie categories and trending titles
    
    feat: Add Login page for user authentication with guest access option
    
    feat: Implement API service for fetching titles and caching responses
    
    feat: Introduce useDebounce and useLazyLoad hooks for optimized performance
    
    style: Add global styles and scrollbar customization for better aesthetics
    
    chore: Configure Vite for development with API proxy setup

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   base: '/',   
+  build: {
+    outDir: 'dist'
+  },
+  define: {
+    global: "window",    
+  },
   
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.imdbapi.dev', // ← api. wala domain
+        target: 'https://api.imdbapi.dev', 
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
